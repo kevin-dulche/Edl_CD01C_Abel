9d97741031b13a78e5280e243376b8bd4fdc18ad
Kevin Dulche Laptop GMR Windows <kevdulche@gmail.com>
2023-11-21 09:26:08 -0600

    Add holaMundo

---
+++ holaMundo/holamundo.py
@@ -0,0 +1,7 @@
+def saludar(nombre, matricula):
+    print(f"Hola Mundo, {nombre}!")
+    print(f"Tu número de matrícula es: {matricula}")
+
+if __name__ == "__main__":
+
+    saludar("Kevin Dulche", 2213026201)
